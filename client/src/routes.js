4ad0a67911375e09510ac46cf3dbb343851f7567
Chibuike <aniakuchibuike@gmail.com>
2020-08-11 01:50:59 +0100

    register

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,7 +7,8 @@ import AddReview from './containers/Admin/add';
 import Login from './containers/Admin/login';
 import User from './components/Admin';
 import UserPosts from './components/Admin/userPost';
-import EditReview from './containers/Admin/edit'
+import EditReview from './containers/Admin/edit';
+import Register from './containers/Admin/register'
 
 import Auth from './hoc/auth';
 
@@ -19,6 +20,7 @@ const Routes = () => {
          <Route path="/login" exact component={Auth(Login,false)}/>
          <Route path="/user" exact component={Auth(User,true)}/> 
          <Route path="/user/add" exact component={Auth(AddReview,true)}/>
+         <Route path="/user/register" exact component={Auth(Register,true)}/>
          <Route path="/user/edit-post/:id" exact component={Auth(EditReview,true)}/>
          <Route path="/books/:id" exact component={Auth(BookView,null)}/>
          <Route path="/user/user-reviews" exact component={Auth(UserPosts,true)}/> 
