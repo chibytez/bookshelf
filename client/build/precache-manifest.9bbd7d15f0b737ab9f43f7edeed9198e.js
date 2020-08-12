e76ceff1f8e53a8b256799df4328929961a15ac3
Chibuike <aniakuchibuike@gmail.com>
2020-08-12 12:37:10 +0100

    build

---
+++ client/build/precache-manifest.9bbd7d15f0b737ab9f43f7edeed9198e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca102a01c261b31a8462a34a1ed30d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66cb930559dbfb6ce8e",
+    "url": "/static/js/2.0ab6b9ef.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.0ab6b9ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8734e1d3dcfbc327c1",
+    "url": "/static/js/main.987f7fbd.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
